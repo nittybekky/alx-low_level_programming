be77758a544680a0bf7fec54a6f3c697e600589b
Goodness <onumah90@gmail.com>
2023-07-26 06:09:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  */
 int main(void)
 {
-	const char *message = 
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	const char *message =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, 59);
 
 	return (1);
