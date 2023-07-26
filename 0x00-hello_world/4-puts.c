4166b7b30c39d7585cf6cc459b98055c1cb0e33f
Goodness <onumah90@gmail.com>
2023-07-26 03:42:23 -0700

    fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle,");
-    return 0;
+/*Program that uses put to display to standard output*/
+int main(void) 
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
