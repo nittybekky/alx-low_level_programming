4c765173c80444547c786d9fc55d2270e664e181
Goodness <onumah90@gmail.com>
2023-07-26 11:17:38 -0700

    fifth commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: 0 is success
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter != 'q' && letter != 'e')
+	{
+		putchar('letter');
+		letter++;
+	}
+
+	putchar('\n');
+
+	return 0;
+}
