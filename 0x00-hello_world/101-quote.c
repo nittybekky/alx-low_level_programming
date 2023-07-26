459aae4268f698e35eb6517fd00d87d357a02062
Goodness <onumah90@gmail.com>
2023-07-26 05:52:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,11 @@
  * main - write to std out without printf or put
  * Return: return 1 on success else 0
  */
-int main(void) {
-	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+int main(void)
+{
+	const char *message = 
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	write(2, message, 59);
+
 	return (1);
 }
