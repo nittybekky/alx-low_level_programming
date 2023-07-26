29b5e971d10f3037438cd293de52934557367608
Goodness <onumah90@gmail.com>
2023-07-26 04:58:56 -0700

    seventh commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints sizes of type for machine it runs on
+ * Return: gives off 0 for success and 1 for failure.
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
