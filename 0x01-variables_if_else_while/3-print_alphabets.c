1cfd01a7a7f54a689eaed50b1044b02efaa4a805
Goodness <onumah90@gmail.com>
2023-07-26 10:19:18 -0700

    fourth commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+	char letter = 'a';
+	char letterBig = 'A'
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	while (letterBig <= 'Z')
+        {
+                putchar(letterBig);
+                letterBig++;
+        }
+
+	putchar('\n');
+
+	return (0);
+}
