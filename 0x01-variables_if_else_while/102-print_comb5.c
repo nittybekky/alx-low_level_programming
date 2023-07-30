53b63511b0b7d5db68bd3508301034a9b90e84f5
Goodness <onumah90@gmail.com>
2023-07-30 16:48:58 -0700

    twelvth commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,18 @@ int main(void)
 {
 int first_num, second_num;
 /* Loop through all possible two-digit combinations */
-for (first_num = 0; first_num <= 99; first_num++)
+for (first_num = 0; first_num <= 98; first_num++)
 {
-for (second_num = 0; second_num <= 99; second_num++)
-{
-/* Check if the two numbers are in ascending order */
-if (first_num <= second_num)
+for (second_num = first_num + 1; second_num <= 99; second_num++)
 {
 /* Print the two-digit combination */
 printf("%02d %02d", first_num, second_num);
 /* Check if it's the last combination */
-if (first_num != 99 || second_num != 99)
+if (first_num != 98 || second_num != 99)
 {
 /* Print the separator */
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
