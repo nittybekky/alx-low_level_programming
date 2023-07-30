67bf24dfa32dd6475eb8509d565591ec77ed5558
Goodness <onumah90@gmail.com>
2023-07-30 15:58:42 -0700

    eleventh commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 /**
-* _custom_strcat - Concatenates two strings.
-* @dest: Pointer to the destination string.
-* @src: Pointer to the source string to be appended to dest.
+* main - Entry point of the program
 *
-* Return: Pointer to the resulting string dest.
+* Return: Always 0 (Success)
 */
-char *_custom_strcat(char *dest, char *src)
-{
-char *dest_end = dest;
-/* Find the end of the destination string */
-while (*dest_end)
-{
-dest_end++;
-}
-/* Append the source string to the destination string */
-while (*src)
-{
-*dest_end = *src;
-dest_end++;
-src++;
-}
-/* Add a terminating null byte at the end of the concatenated string */
-*dest_end = '\0';
-return dest;
-}
 int main(void)
 {
-char dest[50] = "Hello, ";
-char src[] = "World!";
-printf("Before concatenation: %s\n", dest);
-_custom_strcat(dest, src);
-printf("After concatenation: %s\n", dest);
+int first_digit, second_digit;
+/* Loop through all possible two-digit combinations */
+for (first_digit = 0; first_digit <= 9; first_digit++)
+{
+for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+{
+/* Print the two-digit combination */
+putchar('0' + first_digit);
+putchar('0' + second_digit);
+/* Check if it's the last combination */
+if (first_digit != 8 || second_digit != 9)
+{
+/* Print the separator */
+putchar(',');
+putchar(' ');
+}
+}
+}
+/* Print newline at the end */
+putchar('\n');
 return (0);
 }
