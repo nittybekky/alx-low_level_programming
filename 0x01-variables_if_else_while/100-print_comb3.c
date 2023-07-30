ad1b0b073331fbead646c99f537d2aa79a008c12
Goodness <onumah90@gmail.com>
2023-07-30 15:36:00 -0700

    eleventh commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,11 @@
 /**
-* _strcat - Concatenates two strings.
+* _custom_strcat - Concatenates two strings.
 * @dest: Pointer to the destination string.
 * @src: Pointer to the source string to be appended to dest.
 *
 * Return: Pointer to the resulting string dest.
 */
-char *_strcat(char *dest, char *src)
+char *_custom_strcat(char *dest, char *src)
 {
 /* Find the end of the destination string */
 int dest_len = 0;
@@ -14,11 +14,10 @@ while (dest[dest_len] != '\0')
 dest_len++;
 }
 /* Append the source string to the destination string */
-int i = 0;
-while (src[i] != '\0')
+int i; // Declare 'i' at the beginning of the block
+for (i = 0; src[i] != '\0'; i++)
 {
 dest[dest_len + i] = src[i];
-i++;
 }
 /* Add a terminating null byte at the end of the concatenated string */
 dest[dest_len + i] = '\0';
