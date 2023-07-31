c6436590df8ae14f53790e6c3fece72c4f8fc964
Goodness <onumah90@gmail.com>
2023-07-31 10:05:28 -0700

    fourth commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,15 @@
 #include <unistd.h>
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
