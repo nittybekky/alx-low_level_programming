68a1b119d9de515bd61a699014368e528f6ff96b
Goodness <onumah90@gmail.com>
2023-07-31 04:57:16 -0700

    second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,8 @@
 /**
 * print_alphabet - Prints the lowercase alphabet followed by a new line
 *
-* Description: Prints the lowercase alphabet from 'a' to 'z',
-* followed by a new line, using the _putchar function twice.
+* Description: This function prints the lowercase alphabet from 'a' to 'z',
+* followed by a new line, using the putchar function twice.
 */
 void print_alphabet(void)
 {
@@ -11,8 +11,8 @@ char letter;
 /* Loop through the lowercase alphabet and print each letter */
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-_putchar(letter);
+putchar(letter);
 }
 /* Print a new line after printing the alphabet */
-_putchar('\n');
+putchar('\n');
 }
