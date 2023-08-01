85e6c72b1417871ef25d5da1560a548d43749f71
Goodness <onumah90@gmail.com>
2023-08-01 14:54:04 -0700

    1

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,11 +11,11 @@ if (size <= 0)
 _putchar('\n');
 return;
 }
-for (i = 0; i < size; i++)
+for (i = 1; i <= size; i++)
 {
-for (j = 0; j < size - i; j++)
+for (j = 1; j <= size - i; j++)
 _putchar(' ');
-for (j = 0; j <= i; j++)
+for (j = 1; j <= i; j++)
 _putchar('#');
 _putchar('\n');
 }
